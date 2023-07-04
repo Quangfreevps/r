ef15f60f2b70ed9c32b5e77bd2ca9003e1bb3f44
Quangfreevps <127004683+Quangfreevps@users.noreply.github.com>
2023-07-04 16:24:34 +0700

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,4 +1,1 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm64.zip
-unzip ngrok-stable-linux-arm64.zip 
-./ngrok authtoken 2S60NxyuXuEsXzZQRGPHWMnwpeQ_577Y2BkiuyQbXpcddE52a
 ./ngrok http 8080
